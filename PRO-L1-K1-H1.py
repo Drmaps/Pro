82607a0957c3e13bc7f6e4b687dd6268279ea300
Drmaps <96628282+Drmaps@users.noreply.github.com>
2026-01-16 22:25:15 +0300

    PRO-L1-K1-H1.py

---
+++ PRO-L1-K1-H1.py
@@ -0,0 +1,6 @@
+ad = input("Adınızı giriniz :")
+dogum = int(input("Doğduğunuz yılı giriniz:"))
+yil2 = int(input("Yaşınızı merak ettiğiniz yılı giriniz:"))
+yas = yil2 - dogum
+print("Adınız:",ad,
+      "yaşınız:",yas)
